78e714b1d7ff564f46cdaf5ebda1bb57dfe7d431
HB02 <HB02@HB02PC>
2015-10-07 17:59:13 +0900

    ..

--- java-algo/src/loop/NumberBingo.java
+++ java-algo/src/loop/NumberBingo.java
@@ -2,6 +2,8 @@ package loop;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 /**
  * @file_name : NumberBingo.java 
  * @author    : coolbeat@naver.com
@@ -16,16 +18,24 @@ public class NumberBingo {
 	 */
 	public static void main(String[] args) {
 		int com=0,player=0,count=0;
-		com = 0; // 랜덤수 발생
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.println("1부터 10까지 숫자만 넣으시오.");
+		com = 0; // 랜덤수 발생 
+		String result = "";
+		int randomNum = (int) ((Math.random()*10)+1);
+		
+			while (true) {
+			int player2 = Integer.parseInt(JOptionPane.showInputDialog("1부터 10까지 숫자만 넣으시오."));
 			try {
 				count++;
-				// 로직을 넣으시고
+				if (player2 == randomNum) {
+				result = "컴퓨터가 입력한 값"+randomNum+"과 일치합니다.";
+				}else if (player2 > randomNum) {
+					result = "컴퓨터의 값은"+player2+"보다 작은 수 입니다 다시 입력해 주세요";
+				}else {
+					result = "컴퓨터의 값은"+player2+"보다 큰 수 입니다 다시 입력해 주세요";
+				}
 				if(count==3){
 					System.out.println("게임의 총 회수는 3회입니다.");
-					System.out.println("정답은 ");
+					System.out.println("정답은 :"+randomNum);
 					return;
 				}
 			} catch (Exception e) {
@@ -33,8 +43,8 @@ public class NumberBingo {
 				count--; // 범위밖의 숫자를 입력했을 때에는 카운트에서 제외 해준다.
 			}
 			
-			
-		}
+			System.out.println(result);
+		}	
 	}
 }
 
